a7ad114bd4e5fa64ab210cf81b97fd90167f2166
hedaya <hedaya2014selim@gmail.com>
2021-04-12 16:17:30 +0200

    login with angular-oauth2-oidc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { OAuthModule, OAuthStorage } from 'angular-oauth2-oidc';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
+import { BASE_URL } from './app.tokens';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { DirectivesComponent } from './directives/directives.component';
@@ -15,6 +20,15 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { PasswordFlowLoginComponent } from './password-flow-login/password-flow-login.component';
+import {ExtraOptions, RouterModule} from '@angular/router';
+import { useHash } from './flags';
+
+const ROUTING_OPTIONS: ExtraOptions = {
+  // preloadingStrategy: CustomPreloadingStrategy,
+  useHash: useHash,
+  initialNavigation: !useHash
+};
 
 @NgModule({
   declarations: [
@@ -30,13 +44,29 @@ import { ServerElementComponent } from './server-element/server-element.componen
     RecipeDetailComponent,
     RecipeItemComponent,
     CockpitComponent,
-    ServerElementComponent
+    ServerElementComponent,
+    PasswordFlowLoginComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
+    imports: [
+      RouterModule.forRoot(APP_ROUTES, ROUTING_OPTIONS),
+      BrowserModule,
+      FormsModule,
+      RouterModule,
+      HttpClientModule,
+      OAuthModule.forRoot({
+        resourceServer: {
+          allowedUrls: ['http://www.angular.at/api'],
+          sendAccessToken: true
+        }
+      })
+    ],
+  providers: [
+    // (useHash) ? { provide: LocationStrategy, useClass: HashLocationStrategy } : [],
+    // {provide: AuthConfig, useValue: authConfig },
+    // { provide: OAuthStorage, useValue: localStorage },
+    // { provide: ValidationHandler, useClass: JwksValidationHandler },
+    { provide: BASE_URL, useValue: 'http://www.angular.at' }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { PasswordFlowLoginComponent } from './password-flow-login/password-flow-login.component';
+import { Routes, RouterModule } from '@angular/router';
+
+export let APP_ROUTES: Routes = [
+  {
+    path: 'password-flow-login',
+    component: PasswordFlowLoginComponent
+  }
+];
